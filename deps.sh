87ea598a702d9c9bd9c2381e717b844830e17b28
Moritz Firsching <firsching@google.com>
2024-06-07 21:16:52 +0200

    add googletest to deps.sh (#3623)
    Fixes #3621
    
    Co-authored-by: Eugene Kliuchnikov <eustas@google.com>
    (cherry picked from commit ab0bad37135537ef8b40aef4a4d11fe85a878a90)

--- deps.sh
+++ deps.sh
@@ -15,6 +15,7 @@ MYDIR=$(dirname $(realpath "$0"))
 # Git revisions we use for the given submodules. Update these whenever you
 # update a git submodule.
 TESTDATA="873045a9c42ed60721756e26e2a6b32e17415205"
+THIRD_PARTY_GOOGLETEST="58d77fa8070e8cec2dc1ed015d66b454c8d78850"
 THIRD_PARTY_HIGHWAY="58b52a717469e62b2d9b8eaa2f5dddb44d4a4cbf"
 THIRD_PARTY_SKCMS="42030a771244ba67f86b1c1c76a6493f873c5f91"
 THIRD_PARTY_SJPEG="e5ab13008bb214deb66d5f3e17ca2f8dbff150bf"
@@ -81,6 +82,7 @@ EOF
 
   # Sources downloaded from a tarball.
   download_github testdata libjxl/testdata
+  download_github third_party/googletest google/googletest
   download_github third_party/highway google/highway
   download_github third_party/sjpeg webmproject/sjpeg
   download_github third_party/skcms \
