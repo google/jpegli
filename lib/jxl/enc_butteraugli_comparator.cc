35b7dd10dcd0b47fbf0b888a03a5adeb869a75c4
Zoltan Szabadka <szabadka@google.com>
2024-06-17 09:16:14 +0000

    Simplify color transform in butteraugli comparator

--- lib/jxl/enc_butteraugli_comparator.cc
+++ lib/jxl/enc_butteraugli_comparator.cc
@@ -20,51 +20,38 @@ namespace {
 // (gamma-compressed) grayscale background color, alpha image represents
 // weights of the sRGB colors in the [0 .. (1 << bit_depth) - 1] interval,
 // output image is in linear space.
-void AlphaBlend(const Image3F& in, const size_t c, float background_linear,
-                const ImageF& alpha, Image3F* out) {
+void AlphaBlend(float background_linear, const ImageF& alpha, Image3F* inout) {
   const float background = LinearToSrgb8Direct(background_linear);
 
-  for (size_t y = 0; y < out->ysize(); ++y) {
+  for (size_t y = 0; y < inout->ysize(); ++y) {
     const float* JXL_RESTRICT row_a = alpha.ConstRow(y);
-    const float* JXL_RESTRICT row_i = in.ConstPlaneRow(c, y);
-    float* JXL_RESTRICT row_o = out->PlaneRow(c, y);
-    for (size_t x = 0; x < out->xsize(); ++x) {
-      const float a = row_a[x];
-      if (a <= 0.f) {
-        row_o[x] = background_linear;
-      } else if (a >= 1.f) {
-        row_o[x] = row_i[x];
-      } else {
-        const float w_fg = a;
-        const float w_bg = 1.0f - w_fg;
-        const float fg = w_fg * LinearToSrgb8Direct(row_i[x]);
-        const float bg = w_bg * background;
-        row_o[x] = Srgb8ToLinearDirect(fg + bg);
+    for (size_t c = 0; c < 3; ++c) {
+      float* JXL_RESTRICT row = inout->PlaneRow(c, y);
+      for (size_t x = 0; x < inout->xsize(); ++x) {
+        const float a = row_a[x];
+        if (a <= 0.f) {
+          row[x] = background_linear;
+        } else if (a < 1.f) {
+          const float w_fg = a;
+          const float w_bg = 1.0f - w_fg;
+          const float fg = w_fg * LinearToSrgb8Direct(row[x]);
+          const float bg = w_bg * background;
+          row[x] = Srgb8ToLinearDirect(fg + bg);
+        }
       }
     }
   }
 }
 
-void AlphaBlend(float background_linear, ImageBundle* io_linear_srgb) {
-  // No alpha => all opaque.
-  if (!io_linear_srgb->HasAlpha()) return;
-
-  for (size_t c = 0; c < 3; ++c) {
-    AlphaBlend(*io_linear_srgb->color(), c, background_linear,
-               *io_linear_srgb->alpha(), io_linear_srgb->color());
-  }
-}
-
-float ComputeButteraugliImpl(const ImageBundle& ref, const ImageBundle& actual,
-                             const ButteraugliParams& params,
-                             const JxlCmsInterface& cms,
-                             ImageF* distmap) {
+float ComputeButteraugli(const Image3F& ref, const Image3F& actual,
+                         const ButteraugliParams& params,
+                         const JxlCmsInterface& cms,
+                         ImageF* distmap) {
   std::unique_ptr<ButteraugliComparator> comparator;
-  JXL_ASSIGN_OR_DIE(comparator, ButteraugliComparator::Make(
-      ref.color(), params));
+  JXL_ASSIGN_OR_DIE(comparator, ButteraugliComparator::Make(ref, params));
   JXL_ASSIGN_OR_DIE(ImageF temp_diffmap,
                     ImageF::Create(ref.xsize(), ref.ysize()));
-  JXL_CHECK(comparator->Diffmap(actual.color(), temp_diffmap));
+  JXL_CHECK(comparator->Diffmap(actual, temp_diffmap));
   float score = ButteraugliScoreFromDiffmap(temp_diffmap, &params);
   if (distmap != nullptr) {
     distmap->Swap(temp_diffmap);
@@ -72,43 +59,6 @@ float ComputeButteraugliImpl(const ImageBundle& ref, const ImageBundle& actual,
   return score;
 }
 
-Status TransformIfNeeded(const ImageBundle& in, const ColorEncoding& c_desired,
-                         const JxlCmsInterface& cms, ThreadPool* pool,
-                         ImageBundle* store, const ImageBundle** out) {
-  if (in.c_current().SameColorEncoding(c_desired) && !in.HasBlack()) {
-    *out = &in;
-    return true;
-  }
-  // TODO(janwas): avoid copying via createExternal+copyBackToIO
-  // instead of copy+createExternal+copyBackToIO
-  JXL_ASSIGN_OR_RETURN(Image3F color,
-                       Image3F::Create(in.color().xsize(), in.color().ysize()));
-  CopyImageTo(in.color(), &color);
-  store->SetFromImage(std::move(color), in.c_current());
-
-  // Must at least copy the alpha channel for use by external_image.
-  if (in.HasExtraChannels()) {
-    std::vector<ImageF> extra_channels;
-    for (const ImageF& extra_channel : in.extra_channels()) {
-      JXL_ASSIGN_OR_RETURN(ImageF ec, ImageF::Create(extra_channel.xsize(),
-                                                     extra_channel.ysize()));
-      CopyImageTo(extra_channel, &ec);
-      extra_channels.emplace_back(std::move(ec));
-    }
-    store->SetExtraChannels(std::move(extra_channels));
-  }
-
-
-  if (!ApplyColorTransform(
-          store->c_current(), store->metadata()->IntensityTarget(),
-          *store->color(), store->HasBlack() ? &store->black() : nullptr,
-          Rect(*store->color()), c_desired, cms, pool, store->color())) {
-    return false;
-  }
-  store->OverrideProfile(c_desired);
-  *out = store;
-  return true;
-}
 }  // namespace
 
 float ButteraugliDistance(const ImageBundle& rgb0, const ImageBundle& rgb1,
@@ -119,51 +69,70 @@ float ButteraugliDistance(const ImageBundle& rgb0, const ImageBundle& rgb1,
     fprintf(stderr, "Images must have the same size for butteraugli.");
     return std::numeric_limits<float>::max();
   }
+  if (rgb0.IsGray() != rgb1.IsGray()) {
+    fprintf(stderr, "Grayscale vs RGB comparison not supported.");
+    return std::numeric_limits<float>::max();
+  }
+  const size_t xsize = rgb0.xsize();
+  const size_t ysize = rgb0.ysize();
+  const bool is_gray = rgb0.IsGray();
+  ColorEncoding c_desired = ColorEncoding::LinearSRGB(is_gray);
 
-  // Convert to linear sRGB (unless already in that space)
-  ImageMetadata metadata0 = *rgb0.metadata();
-  ImageBundle store0(&metadata0);
-  const ImageBundle* linear_srgb0;
-  JXL_CHECK(TransformIfNeeded(rgb0, ColorEncoding::LinearSRGB(rgb0.IsGray()),
-                              cms, pool, &store0, &linear_srgb0));
-  ImageMetadata metadata1 = *rgb1.metadata();
-  ImageBundle store1(&metadata1);
-  const ImageBundle* linear_srgb1;
-  JXL_CHECK(TransformIfNeeded(rgb1, ColorEncoding::LinearSRGB(rgb1.IsGray()),
-                              cms, pool, &store1, &linear_srgb1));
+  JXL_ASSIGN_OR_DIE(Image3F linear_srgb0, Image3F::Create(xsize, ysize));
+  if (rgb0.c_current().SameColorEncoding(c_desired) && !rgb0.HasBlack()) {
+    CopyImageTo(rgb0.color(), &linear_srgb0);
+  } else{
+    JXL_CHECK(ApplyColorTransform(
+        rgb0.c_current(), rgb0.metadata()->IntensityTarget(),
+        rgb0.color(), rgb0.HasBlack() ? &rgb0.black() : nullptr,
+        Rect(rgb0.color()), c_desired, cms, pool, &linear_srgb0));
+  }
+  JXL_ASSIGN_OR_DIE(Image3F linear_srgb1, Image3F::Create(xsize, ysize));
+  if (rgb1.c_current().SameColorEncoding(c_desired) && !rgb1.HasBlack()) {
+    CopyImageTo(rgb1.color(), &linear_srgb1);
+  } else{
+    JXL_CHECK(ApplyColorTransform(
+        rgb1.c_current(), rgb1.metadata()->IntensityTarget(),
+        rgb1.color(), rgb1.HasBlack() ? &rgb1.black() : nullptr,
+        Rect(rgb1.color()), c_desired, cms, pool, &linear_srgb1));
+  }
 
   // No alpha: skip blending, only need a single call to Butteraugli.
   if (ignore_alpha || (!rgb0.HasAlpha() && !rgb1.HasAlpha())) {
-    return ComputeButteraugliImpl(
-        *linear_srgb0, *linear_srgb1, params, cms, diffmap);
+    return ComputeButteraugli(linear_srgb0, linear_srgb1, params, cms, diffmap);
   }
 
   // Blend on black and white backgrounds
 
   const float black = 0.0f;
-  JXL_ASSIGN_OR_DIE(ImageBundle blended_black0, linear_srgb0->Copy());
-  JXL_ASSIGN_OR_DIE(ImageBundle blended_black1, linear_srgb1->Copy());
-  AlphaBlend(black, &blended_black0);
-  AlphaBlend(black, &blended_black1);
-
   const float white = 1.0f;
-  JXL_ASSIGN_OR_DIE(ImageBundle blended_white0, linear_srgb0->Copy());
-  JXL_ASSIGN_OR_DIE(ImageBundle blended_white1, linear_srgb1->Copy());
+  JXL_ASSIGN_OR_DIE(Image3F blended_black0, Image3F::Create(xsize, ysize));
+  JXL_ASSIGN_OR_DIE(Image3F blended_white0, Image3F::Create(xsize, ysize));
+  CopyImageTo(linear_srgb0, &blended_black0);
+  CopyImageTo(linear_srgb0, &blended_white0);
+  if (rgb0.HasAlpha()) {
+    AlphaBlend(black, rgb0.alpha(), &blended_black0);
+    AlphaBlend(white, rgb0.alpha(), &blended_white0);
+  }
 
-  AlphaBlend(white, &blended_white0);
-  AlphaBlend(white, &blended_white1);
+  JXL_ASSIGN_OR_DIE(Image3F blended_black1, Image3F::Create(xsize, ysize));
+  JXL_ASSIGN_OR_DIE(Image3F blended_white1, Image3F::Create(xsize, ysize));
+  CopyImageTo(linear_srgb1, &blended_black1);
+  CopyImageTo(linear_srgb1, &blended_white1);
+  if (rgb1.HasAlpha()) {
+    AlphaBlend(black, rgb1.alpha(), &blended_black1);
+    AlphaBlend(white, rgb1.alpha(), &blended_white1);
+  }
 
   ImageF diffmap_black;
   ImageF diffmap_white;
-  const float dist_black = ComputeButteraugliImpl(
+  const float dist_black = ComputeButteraugli(
       blended_black0, blended_black1, params, cms, &diffmap_black);
-  const float dist_white = ComputeButteraugliImpl(
+  const float dist_white = ComputeButteraugli(
       blended_white0, blended_white1, params, cms, &diffmap_white);
 
   // diffmap and return values are the max of diffmap_black/white.
   if (diffmap != nullptr) {
-    const size_t xsize = rgb0.xsize();
-    const size_t ysize = rgb0.ysize();
     JXL_ASSIGN_OR_DIE(*diffmap, ImageF::Create(xsize, ysize));
     for (size_t y = 0; y < ysize; ++y) {
       const float* JXL_RESTRICT row_black = diffmap_black.ConstRow(y);
