libjxl/libjxl

Isolate move FUZZ_TEST and MemoryManager() out of test_utils (#3617)

lib/jxl/butteraugli/butteraugli_test.cc
@@ -20,7 +20,7 @@
 #include "lib/jxl/image.h"
 #include "lib/jxl/image_ops.h"
 #include "lib/jxl/test_image.h"
-#include "lib/jxl/test_utils.h"
+#include "lib/jxl/test_memory_manager.h"
 #include "lib/jxl/testing.h"
 
 namespace jxl {

lib/jxl_lists.bzl
@@ -524,6 +524,7 @@ libjxl_jpegli_sources = [
 ]
 
 libjxl_jpegli_testlib_files = [
+    "jpegli/fuzztest.h",
     "jpegli/test_params.h",
     "jpegli/test_utils-inl.h",
     "jpegli/test_utils.cc",
@@ -571,10 +572,13 @@ libjxl_testlib_files = [
     "jxl/dec_transforms_testonly.cc",
     "jxl/dec_transforms_testonly.h",
     "jxl/fake_parallel_runner_testonly.h",
+    "jxl/fuzztest.h",
     "jxl/image_test_utils.h",
     "jxl/render_pipeline/test_render_pipeline_stages.h",
     "jxl/test_image.cc",
     "jxl/test_image.h",
+    "jxl/test_memory_manager.cc",
+    "jxl/test_memory_manager.h",
     "jxl/test_utils.cc",
     "jxl/test_utils.h",
 ]

lib/jxl_lists.cmake
@@ -519,6 +519,7 @@ set(JPEGXL_INTERNAL_JPEGLI_SOURCES
 )
 
 set(JPEGXL_INTERNAL_JPEGLI_TESTLIB_FILES
+  jpegli/fuzztest.h
   jpegli/test_params.h
   jpegli/test_utils-inl.h
   jpegli/test_utils.cc
@@ -560,10 +561,13 @@ set(JPEGXL_INTERNAL_TESTLIB_FILES
   jxl/dec_transforms_testonly.cc
   jxl/dec_transforms_testonly.h
   jxl/fake_parallel_runner_testonly.h
+  jxl/fuzztest.h
   jxl/image_test_utils.h
   jxl/render_pipeline/test_render_pipeline_stages.h
   jxl/test_image.cc
   jxl/test_image.h
+  jxl/test_memory_manager.cc
+  jxl/test_memory_manager.h
   jxl/test_utils.cc
   jxl/test_utils.h
 )

tools/gauss_blur_test.cc
@@ -14,7 +14,7 @@
 #include "lib/jxl/base/printf_macros.h"
 #include "lib/jxl/image_ops.h"
 #include "lib/jxl/image_test_utils.h"
-#include "lib/jxl/test_utils.h"
+#include "lib/jxl/test_memory_manager.h"
 #include "lib/jxl/testing.h"
 
 namespace jxl {

google/jpegli

(cherry picked from commit c8e435cbe70fac9679d05367e8297c0a27aacd13)

lib/extras/butteraugli_test.cc
@@ -23,6 +23,7 @@
 #include "lib/extras/packed_image.h"
 #include "lib/extras/packed_image_convert.h"
 #include "lib/extras/test_image.h"
+#include "lib/extras/test_memory_manager.h"
 #include "lib/extras/test_utils.h"
 
 namespace jxl {

lib/jxl_lists.bzl
@@ -529,6 +529,7 @@ libjxl_jpegli_sources = [
 ]
 
 libjxl_jpegli_testlib_files = [
+    "jpegli/fuzztest.h",
     "jpegli/test_params.h",
     "jpegli/test_utils-inl.h",
     "jpegli/test_utils.cc",
@@ -574,6 +575,8 @@ libjxl_public_headers = [
 libjxl_testlib_files = [
     "extras/test_image.cc",
     "extras/test_image.h",
+    "extras/test_memory_manager.cc",
+    "extras/test_memory_manager.h",
     "extras/test_utils.cc",
     "extras/test_utils.h",
     "threads/test_utils.h",

lib/jxl_lists.cmake
@@ -519,6 +519,7 @@ set(JPEGXL_INTERNAL_JPEGLI_SOURCES
 )
 
 set(JPEGXL_INTERNAL_JPEGLI_TESTLIB_FILES
+  jpegli/fuzztest.h
   jpegli/test_params.h
   jpegli/test_utils-inl.h
   jpegli/test_utils.cc
@@ -543,6 +544,8 @@ set(JPEGXL_INTERNAL_JPEGLI_WRAPPER_SOURCES
 set(JPEGXL_INTERNAL_TESTLIB_FILES
   extras/test_image.cc
   extras/test_image.h
+  extras/test_memory_manager.cc
+  extras/test_memory_manager.h
   extras/test_utils.cc
   extras/test_utils.h
   threads/test_utils.h

tools/gauss_blur_test.cc
@@ -16,6 +16,7 @@
 #include "lib/base/testing.h"
 #include "lib/extras/image_ops.h"
 #include "lib/extras/test_utils.h"
+#include "lib/extras/test_memory_manager.h"
 
 namespace jxl {
 