libjxl/libjxl

Actually use JxlMemoryManager (#3582)

lib/jxl/image.h
@@ -23,7 +23,7 @@
 
 #include "lib/jxl/base/compiler_specific.h"
 #include "lib/jxl/base/status.h"
-#include "lib/jxl/cache_aligned.h"
+#include "lib/jxl/memory_manager_internal.h"
 
 namespace jxl {
 
@@ -39,8 +39,6 @@ struct PlaneBase {
         orig_xsize_(0),
         orig_ysize_(0),
         bytes_per_row_(0),
-        memory_manager_(nullptr),
-        bytes_(nullptr),
         sizeof_t_(0) {}
 
   // Copy construction/assignment is forbidden to avoid inadvertent copies,
@@ -54,11 +52,7 @@ struct PlaneBase {
   // Move assignment (required for std::vector)
   PlaneBase& operator=(PlaneBase&& other) noexcept = default;
 
-  ~PlaneBase() {
-    if (bytes_.get()) {
-      JXL_CHECK(memory_manager_);
-    }
-  }
+  ~PlaneBase() {}
 
   void Swap(PlaneBase& other);
 
@@ -83,17 +77,17 @@ struct PlaneBase {
   JXL_INLINE size_t bytes_per_row() const { return bytes_per_row_; }
 
   JXL_INLINE JxlMemoryManager* memory_manager() const {
-    return memory_manager_;
+    return bytes_.memory_manager();
   }
 
   // Raw access to byte contents, for interfacing with other libraries.
   // Unsigned char instead of char to avoid surprises (sign extension).
   JXL_INLINE uint8_t* bytes() {
-    void* p = bytes_.get();
+    uint8_t* p = bytes_.address<uint8_t>();
     return static_cast<uint8_t * JXL_RESTRICT>(JXL_ASSUME_ALIGNED(p, 64));
   }
   JXL_INLINE const uint8_t* bytes() const {
-    const void* p = bytes_.get();
+    const uint8_t* p = bytes_.address<uint8_t>();
     return static_cast<const uint8_t * JXL_RESTRICT>(JXL_ASSUME_ALIGNED(p, 64));
   }
 
@@ -111,7 +105,7 @@ struct PlaneBase {
     }
 #endif
 
-    void* row = bytes_.get() + y * bytes_per_row_;
+    uint8_t* row = bytes_.address<uint8_t>() + y * bytes_per_row_;
     return JXL_ASSUME_ALIGNED(row, 64);
   }
 
@@ -121,8 +115,7 @@ struct PlaneBase {
   uint32_t orig_xsize_;
   uint32_t orig_ysize_;
   size_t bytes_per_row_;  // Includes padding.
-  JxlMemoryManager* memory_manager_;
-  CacheAlignedUniquePtr bytes_;
+  AlignedMemory bytes_;
   size_t sizeof_t_;
 };
 
@@ -223,17 +216,16 @@ class Image3 {
 
   Image3() : planes_{PlaneT(), PlaneT(), PlaneT()} {}
 
-  Image3(Image3&& other) noexcept {
-    for (size_t i = 0; i < kNumPlanes; i++) {
-      planes_[i] = std::move(other.planes_[i]);
-    }
-  }
-
   // Copy construction/assignment is forbidden to avoid inadvertent copies,
   // which can be very expensive. Use CopyImageTo instead.
   Image3(const Image3& other) = delete;
   Image3& operator=(const Image3& other) = delete;
 
+  Image3(Image3&& other) noexcept {
+    for (size_t i = 0; i < kNumPlanes; i++) {
+      planes_[i] = std::move(other.planes_[i]);
+    }
+  }
   Image3& operator=(Image3&& other) noexcept {
     for (size_t i = 0; i < kNumPlanes; i++) {
       planes_[i] = std::move(other.planes_[i]);

lib/jxl/memory_manager_internal.cc
@@ -5,14 +5,160 @@
 
 #include "lib/jxl/memory_manager_internal.h"
 
-#include <stdlib.h>
+#include <jxl/memory_manager.h>
+#include <jxl/types.h>
+
+#include <atomic>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>     // memcpy
+#include <hwy/base.h>  // kMaxVectorSize
+
+#include "lib/jxl/base/common.h"
+#include "lib/jxl/base/status.h"
+#include "lib/jxl/simd_util.h"
 
 namespace jxl {
+
+namespace {
+
+// To avoid RFOs, match L2 fill size (pairs of lines); 2 x cache line size.
+constexpr size_t kAlignment = 2 * 64;
+static_assert((kAlignment & (kAlignment - 1)) == 0,
+              "kAlignment must be a power of 2");
+
+// Minimum multiple for which cache set conflicts and/or loads blocked by
+// preceding stores can occur.
+constexpr size_t kNumAlignmentGroups = 16;
+constexpr size_t kAlias = kNumAlignmentGroups * kAlignment;
+static_assert((kNumAlignmentGroups & (kNumAlignmentGroups - 1)) == 0,
+              "kNumAlignmentGroups must be a power of 2");
 
 void* MemoryManagerDefaultAlloc(void* opaque, size_t size) {
   return malloc(size);
 }
 
 void MemoryManagerDefaultFree(void* opaque, void* address) { free(address); }
+
+}  // namespace
+
+void* MemoryManagerAlloc(const JxlMemoryManager* memory_manager, size_t size) {
+  return memory_manager->alloc(memory_manager->opaque, size);
+}
+
+void MemoryManagerFree(const JxlMemoryManager* memory_manager, void* address) {
+  memory_manager->free(memory_manager->opaque, address);
+}
+
+Status MemoryManagerInit(JxlMemoryManager* self,
+                         const JxlMemoryManager* memory_manager) {
+  if (memory_manager) {
+    *self = *memory_manager;
+  } else {
+    memset(self, 0, sizeof(*self));
+  }
+  bool is_default_alloc = (self->alloc == nullptr);
+  bool is_default_free = (self->free == nullptr);
+  if (is_default_alloc != is_default_free) {
+    return false;
+  }
+  if (is_default_alloc) self->alloc = jxl::MemoryManagerDefaultAlloc;
+  if (is_default_free) self->free = jxl::MemoryManagerDefaultFree;
+
+  return true;
+}
+
+size_t BytesPerRow(const size_t xsize, const size_t sizeof_t) {
+  // Special case: we don't allow any ops -> don't need extra padding/
+  if (xsize == 0) {
+    return 0;
+  }
+
+  const size_t vec_size = MaxVectorSize();
+  size_t valid_bytes = xsize * sizeof_t;
+
+  // Allow unaligned accesses starting at the last valid value.
+  // Skip for the scalar case because no extra lanes will be loaded.
+  if (vec_size != 0) {
+    valid_bytes += vec_size - sizeof_t;
+  }
+
+  // Round up to vector and cache line size.
+  const size_t align = std::max(vec_size, kAlignment);
+  size_t bytes_per_row = RoundUpTo(valid_bytes, align);
+
+  // During the lengthy window before writes are committed to memory, CPUs
+  // guard against read after write hazards by checking the address, but
+  // only the lower 11 bits. We avoid a false dependency between writes to
+  // consecutive rows by ensuring their sizes are not multiples of 2 KiB.
+  // Avoid2K prevents the same problem for the planes of an Image3.
+  if (bytes_per_row % kAlias == 0) {
+    bytes_per_row += align;
+  }
+
+  JXL_ASSERT(bytes_per_row % align == 0);
+  return bytes_per_row;
+}
+
+StatusOr<AlignedMemory> AlignedMemory::Create(JxlMemoryManager* memory_manager,
+                                              size_t size) {
+  size_t allocation_size = size + kAlias;
+  if (size > allocation_size) {
+    return JXL_FAILURE("Requested allocation is too large");
+  }
+  JXL_CHECK(memory_manager);
+  void* allocated =
+      memory_manager->alloc(memory_manager->opaque, allocation_size);
+  if (allocated == nullptr) {
+    return JXL_FAILURE("Allocation failed");
+  }
+  return AlignedMemory{memory_manager, allocated};
+}
+
+AlignedMemory::AlignedMemory(JxlMemoryManager* memory_manager, void* allocation)
+    : allocation_(allocation), memory_manager_(memory_manager) {
+  // Congruence to `offset` (mod kAlias) reduces cache conflicts and load/store
+  // stalls, especially with large allocations that would otherwise have similar
+  // alignments.
+  static std::atomic<uint32_t> next_group{0};
+  size_t group =
+      static_cast<size_t>(next_group.fetch_add(1, std::memory_order_relaxed));
+  group &= (kNumAlignmentGroups - 1);
+  size_t offset = kAlignment * group;
+
+  // Actual allocation.
+  uintptr_t address = reinterpret_cast<uintptr_t>(allocation);
+
+  // Aligned address, but might land before allocation (50%/50%).
+  uintptr_t aligned_address = (address & ~(kAlias - 1)) + offset;
+  if (aligned_address < address) aligned_address += kAlias;
+
+  address_ = reinterpret_cast<void*>(aligned_address);  // NOLINT
+}
+
+AlignedMemory::AlignedMemory(AlignedMemory&& other) noexcept {
+  allocation_ = other.allocation_;
+  memory_manager_ = other.memory_manager_;
+  address_ = other.address_;
+  other.memory_manager_ = nullptr;
+}
+
+AlignedMemory& AlignedMemory::operator=(AlignedMemory&& other) noexcept {
+  if (this == &other) return *this;
+  if (memory_manager_ && allocation_) {
+    memory_manager_->free(memory_manager_->opaque, allocation_);
+  }
+  allocation_ = other.allocation_;
+  memory_manager_ = other.memory_manager_;
+  address_ = other.address_;
+  other.memory_manager_ = nullptr;
+  return *this;
+}
+
+AlignedMemory::~AlignedMemory() {
+  if (memory_manager_ == nullptr) return;
+  memory_manager_->free(memory_manager_->opaque, allocation_);
+}
 
 }  // namespace jxl

tools/benchmark/benchmark_xl.cc
@@ -20,6 +20,7 @@
 #include <numeric>
 #include <string>
 #include <thread>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
@@ -37,7 +38,6 @@
 #include "lib/jxl/base/span.h"
 #include "lib/jxl/base/status.h"
 #include "lib/jxl/butteraugli/butteraugli.h"
-#include "lib/jxl/cache_aligned.h"
 #include "lib/jxl/codec_in_out.h"
 #include "lib/jxl/color_encoding_internal.h"
 #include "lib/jxl/enc_butteraugli_comparator.h"
@@ -46,6 +46,7 @@
 #include "lib/jxl/image_bundle.h"
 #include "lib/jxl/image_ops.h"
 #include "lib/jxl/jpeg/enc_jpeg_data.h"
+#include "lib/jxl/memory_manager_internal.h"
 #include "tools/benchmark/benchmark_args.h"
 #include "tools/benchmark/benchmark_codec.h"
 #include "tools/benchmark/benchmark_file_io.h"
@@ -74,6 +75,64 @@ using ::jxl::Rng;
 using ::jxl::Status;
 using ::jxl::ThreadPool;
 using ::jxl::extras::PackedPixelFile;
+
+class TrackingMemoryManager {
+ public:
+  explicit TrackingMemoryManager(JxlMemoryManager* inner) : inner_(inner) {
+    outer_.opaque = reinterpret_cast<void*>(this);
+    outer_.alloc = &Alloc;
+    outer_.free = &Free;
+  }
+
+  JxlMemoryManager* get() { return &outer_; }
+
+  void PrintStats() const {
+    fprintf(stderr, "Allocations: %" PRIuS " (max bytes in use: %E)\n",
+            static_cast<size_t>(num_allocations_),
+            static_cast<double>(max_bytes_in_use_));
+  }
+
+ private:
+  static void* Alloc(void* opaque, size_t size) {
+    JXL_CHECK(opaque != nullptr);
+    TrackingMemoryManager* self =
+        reinterpret_cast<TrackingMemoryManager*>(opaque);
+    void* result = self->inner_->alloc(self->inner_->opaque, size);
+    if (result != nullptr) {
+      std::lock_guard<std::mutex> guard(self->mutex_);
+      self->num_allocations_++;
+      self->bytes_in_use_ += size;
+      self->max_bytes_in_use_ =
+          std::max(self->max_bytes_in_use_, self->bytes_in_use_);
+      self->allocations_[result] = size;
+    }
+    return result;
+  }
+
+  static void Free(void* opaque, void* address) {
+    JXL_CHECK(opaque != nullptr);
+    if (address == nullptr) return;
+    TrackingMemoryManager* self =
+        reinterpret_cast<TrackingMemoryManager*>(opaque);
+    {
+      std::lock_guard<std::mutex> guard(self->mutex_);
+      auto entry = self->allocations_.find(address);
+      JXL_CHECK(entry != self->allocations_.end());
+      self->num_allocations_--;
+      self->bytes_in_use_ -= entry->second;
+      self->allocations_.erase(entry);
+    }
+    self->inner_->free(self->inner_->opaque, address);
+  }
+
+  std::unordered_map<void*, size_t> allocations_;
+  std::mutex mutex_;
+  uint64_t bytes_in_use_ = 0;
+  uint64_t max_bytes_in_use_ = 0;
+  uint64_t num_allocations_ = 0;
+  JxlMemoryManager outer_;
+  JxlMemoryManager* inner_;
+};
 
 Status WriteImage(const Image3F& image, ThreadPool* pool,
                   const std::string& filename) {
@@ -796,13 +855,17 @@ class Benchmark {
   // Return the exit code of the program.
   static int Run() {
     int ret = EXIT_SUCCESS;
+    JxlMemoryManager default_memory_manager;
+    JXL_CHECK(jxl::MemoryManagerInit(&default_memory_manager, nullptr));
+    TrackingMemoryManager memory_manager(&default_memory_manager);
     {
       const StringVec methods = GetMethods();
       const StringVec extra_metrics_names = GetExtraMetricsNames();
       const StringVec extra_metrics_commands = GetExtraMetricsCommands();
       const StringVec fnames = GetFilenames();
       // (non-const because Task.stats are updated)
-      std::vector<Task> tasks = CreateTasks(methods, fnames);
+      std::vector<Task> tasks =
+          CreateTasks(methods, fnames, memory_manager.get());
 
       std::unique_ptr<ThreadPoolInternal> pool;
       std::vector<std::unique_ptr<ThreadPoolInternal>> inner_pools;
@@ -825,7 +888,7 @@ class Benchmark {
       }
     }
 
-    jxl::CacheAligned::PrintStats();
+    memory_manager.PrintStats();
     return ret;
   }
 
@@ -1044,14 +1107,15 @@ class Benchmark {
   }
 
   static std::vector<Task> CreateTasks(const StringVec& methods,
-                                       const StringVec& fnames) {
+                                       const StringVec& fnames,
+                                       JxlMemoryManager* memory_manager) {
     std::vector<Task> tasks;
     tasks.reserve(methods.size() * fnames.size());
     for (size_t idx_image = 0; idx_image < fnames.size(); ++idx_image) {
       for (size_t idx_method = 0; idx_method < methods.size(); ++idx_method) {
         tasks.emplace_back();
         Task& t = tasks.back();
-        t.codec = CreateImageCodec(methods[idx_method]);
+        t.codec = CreateImageCodec(methods[idx_method], memory_manager);
         t.idx_image = idx_image;
         t.idx_method = idx_method;
         // t.stats is default-initialized.

google/jpegli

(cherry picked from commit 8835f5dfc4857f46277f6c703e47ac7817bd7aed)

lib/extras/image.h
@@ -23,7 +23,7 @@
 #include "lib/base/compiler_specific.h"
 #include "lib/base/memory_manager.h"
 #include "lib/base/status.h"
-#include "lib/extras/cache_aligned.h"
+#include "lib/extras/memory_manager_internal.h"
 
 namespace jxl {
 
@@ -39,8 +39,6 @@ struct PlaneBase {
         orig_xsize_(0),
         orig_ysize_(0),
         bytes_per_row_(0),
-        memory_manager_(nullptr),
-        bytes_(nullptr),
         sizeof_t_(0) {}
 
   // Copy construction/assignment is forbidden to avoid inadvertent copies,
@@ -54,11 +52,7 @@ struct PlaneBase {
   // Move assignment (required for std::vector)
   PlaneBase& operator=(PlaneBase&& other) noexcept = default;
 
-  ~PlaneBase() {
-    if (bytes_.get()) {
-      JXL_CHECK(memory_manager_);
-    }
-  }
+  ~PlaneBase() {}
 
   void Swap(PlaneBase& other);
 
@@ -83,17 +77,17 @@ struct PlaneBase {
   JXL_INLINE size_t bytes_per_row() const { return bytes_per_row_; }
 
   JXL_INLINE JxlMemoryManager* memory_manager() const {
-    return memory_manager_;
+    return bytes_.memory_manager();
   }
 
   // Raw access to byte contents, for interfacing with other libraries.
   // Unsigned char instead of char to avoid surprises (sign extension).
   JXL_INLINE uint8_t* bytes() {
-    void* p = bytes_.get();
+    uint8_t* p = bytes_.address<uint8_t>();
     return static_cast<uint8_t * JXL_RESTRICT>(JXL_ASSUME_ALIGNED(p, 64));
   }
   JXL_INLINE const uint8_t* bytes() const {
-    const void* p = bytes_.get();
+    const uint8_t* p = bytes_.address<uint8_t>();
     return static_cast<const uint8_t * JXL_RESTRICT>(JXL_ASSUME_ALIGNED(p, 64));
   }
 
@@ -111,7 +105,7 @@ struct PlaneBase {
     }
 #endif
 
-    void* row = bytes_.get() + y * bytes_per_row_;
+    uint8_t* row = bytes_.address<uint8_t>() + y * bytes_per_row_;
     return JXL_ASSUME_ALIGNED(row, 64);
   }
 
@@ -121,8 +115,7 @@ struct PlaneBase {
   uint32_t orig_xsize_;
   uint32_t orig_ysize_;
   size_t bytes_per_row_;  // Includes padding.
-  JxlMemoryManager* memory_manager_;
-  CacheAlignedUniquePtr bytes_;
+  AlignedMemory bytes_;
   size_t sizeof_t_;
 };
 
@@ -223,17 +216,16 @@ class Image3 {
 
   Image3() : planes_{PlaneT(), PlaneT(), PlaneT()} {}
 
-  Image3(Image3&& other) noexcept {
-    for (size_t i = 0; i < kNumPlanes; i++) {
-      planes_[i] = std::move(other.planes_[i]);
-    }
-  }
-
   // Copy construction/assignment is forbidden to avoid inadvertent copies,
   // which can be very expensive. Use CopyImageTo instead.
   Image3(const Image3& other) = delete;
   Image3& operator=(const Image3& other) = delete;
 
+  Image3(Image3&& other) noexcept {
+    for (size_t i = 0; i < kNumPlanes; i++) {
+      planes_[i] = std::move(other.planes_[i]);
+    }
+  }
   Image3& operator=(Image3&& other) noexcept {
     for (size_t i = 0; i < kNumPlanes; i++) {
       planes_[i] = std::move(other.planes_[i]);

lib/extras/memory_manager_internal.cc
@@ -1,0 +1,162 @@
+// Copyright (c) the JPEG XL Project Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "lib/extras/memory_manager_internal.h"
+
+#include <atomic>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>     // memcpy
+#include <hwy/base.h>  // kMaxVectorSize
+
+#include "lib/base/common.h"
+#include "lib/base/memory_manager.h"
+#include "lib/base/status.h"
+#include "lib/extras/simd_util.h"
+
+namespace jxl {
+
+namespace {
+
+// To avoid RFOs, match L2 fill size (pairs of lines); 2 x cache line size.
+constexpr size_t kAlignment = 2 * 64;
+static_assert((kAlignment & (kAlignment - 1)) == 0,
+              "kAlignment must be a power of 2");
+
+// Minimum multiple for which cache set conflicts and/or loads blocked by
+// preceding stores can occur.
+constexpr size_t kNumAlignmentGroups = 16;
+constexpr size_t kAlias = kNumAlignmentGroups * kAlignment;
+static_assert((kNumAlignmentGroups & (kNumAlignmentGroups - 1)) == 0,
+              "kNumAlignmentGroups must be a power of 2");
+
+void* MemoryManagerDefaultAlloc(void* opaque, size_t size) {
+  return malloc(size);
+}
+
+void MemoryManagerDefaultFree(void* opaque, void* address) { free(address); }
+
+}  // namespace
+
+void* MemoryManagerAlloc(const JxlMemoryManager* memory_manager, size_t size) {
+  return memory_manager->alloc(memory_manager->opaque, size);
+}
+
+void MemoryManagerFree(const JxlMemoryManager* memory_manager, void* address) {
+  memory_manager->free(memory_manager->opaque, address);
+}
+
+Status MemoryManagerInit(JxlMemoryManager* self,
+                         const JxlMemoryManager* memory_manager) {
+  if (memory_manager) {
+    *self = *memory_manager;
+  } else {
+    memset(self, 0, sizeof(*self));
+  }
+  bool is_default_alloc = (self->alloc == nullptr);
+  bool is_default_free = (self->free == nullptr);
+  if (is_default_alloc != is_default_free) {
+    return false;
+  }
+  if (is_default_alloc) self->alloc = jxl::MemoryManagerDefaultAlloc;
+  if (is_default_free) self->free = jxl::MemoryManagerDefaultFree;
+
+  return true;
+}
+
+size_t BytesPerRow(const size_t xsize, const size_t sizeof_t) {
+  // Special case: we don't allow any ops -> don't need extra padding/
+  if (xsize == 0) {
+    return 0;
+  }
+
+  const size_t vec_size = MaxVectorSize();
+  size_t valid_bytes = xsize * sizeof_t;
+
+  // Allow unaligned accesses starting at the last valid value.
+  // Skip for the scalar case because no extra lanes will be loaded.
+  if (vec_size != 0) {
+    valid_bytes += vec_size - sizeof_t;
+  }
+
+  // Round up to vector and cache line size.
+  const size_t align = std::max(vec_size, kAlignment);
+  size_t bytes_per_row = RoundUpTo(valid_bytes, align);
+
+  // During the lengthy window before writes are committed to memory, CPUs
+  // guard against read after write hazards by checking the address, but
+  // only the lower 11 bits. We avoid a false dependency between writes to
+  // consecutive rows by ensuring their sizes are not multiples of 2 KiB.
+  // Avoid2K prevents the same problem for the planes of an Image3.
+  if (bytes_per_row % kAlias == 0) {
+    bytes_per_row += align;
+  }
+
+  JXL_ASSERT(bytes_per_row % align == 0);
+  return bytes_per_row;
+}
+
+StatusOr<AlignedMemory> AlignedMemory::Create(JxlMemoryManager* memory_manager,
+                                              size_t size) {
+  size_t allocation_size = size + kAlias;
+  if (size > allocation_size) {
+    return JXL_FAILURE("Requested allocation is too large");
+  }
+  JXL_CHECK(memory_manager);
+  void* allocated =
+      memory_manager->alloc(memory_manager->opaque, allocation_size);
+  if (allocated == nullptr) {
+    return JXL_FAILURE("Allocation failed");
+  }
+  return AlignedMemory{memory_manager, allocated};
+}
+
+AlignedMemory::AlignedMemory(JxlMemoryManager* memory_manager, void* allocation)
+    : allocation_(allocation), memory_manager_(memory_manager) {
+  // Congruence to `offset` (mod kAlias) reduces cache conflicts and load/store
+  // stalls, especially with large allocations that would otherwise have similar
+  // alignments.
+  static std::atomic<uint32_t> next_group{0};
+  size_t group =
+      static_cast<size_t>(next_group.fetch_add(1, std::memory_order_relaxed));
+  group &= (kNumAlignmentGroups - 1);
+  size_t offset = kAlignment * group;
+
+  // Actual allocation.
+  uintptr_t address = reinterpret_cast<uintptr_t>(allocation);
+
+  // Aligned address, but might land before allocation (50%/50%).
+  uintptr_t aligned_address = (address & ~(kAlias - 1)) + offset;
+  if (aligned_address < address) aligned_address += kAlias;
+
+  address_ = reinterpret_cast<void*>(aligned_address);  // NOLINT
+}
+
+AlignedMemory::AlignedMemory(AlignedMemory&& other) noexcept {
+  allocation_ = other.allocation_;
+  memory_manager_ = other.memory_manager_;
+  address_ = other.address_;
+  other.memory_manager_ = nullptr;
+}
+
+AlignedMemory& AlignedMemory::operator=(AlignedMemory&& other) noexcept {
+  if (this == &other) return *this;
+  if (memory_manager_ && allocation_) {
+    memory_manager_->free(memory_manager_->opaque, allocation_);
+  }
+  allocation_ = other.allocation_;
+  memory_manager_ = other.memory_manager_;
+  address_ = other.address_;
+  other.memory_manager_ = nullptr;
+  return *this;
+}
+
+AlignedMemory::~AlignedMemory() {
+  if (memory_manager_ == nullptr) return;
+  memory_manager_->free(memory_manager_->opaque, allocation_);
+}
+
+}  // namespace jxl

tools/benchmark/benchmark_xl.cc
@@ -15,6 +15,7 @@
 #include <numeric>
 #include <string>
 #include <thread>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
@@ -36,6 +37,7 @@
 #include "lib/extras/enc/pnm.h"
 #include "lib/extras/image.h"
 #include "lib/extras/image_ops.h"
+#include "lib/extras/memory_manager_internal.h"
 #include "lib/extras/metrics.h"
 #include "lib/extras/packed_image.h"
 #include "lib/extras/packed_image_convert.h"
@@ -63,6 +65,64 @@ using ::jxl::Rng;
 using ::jxl::Status;
 using ::jxl::ThreadPool;
 using ::jxl::extras::PackedPixelFile;
+
+class TrackingMemoryManager {
+ public:
+  explicit TrackingMemoryManager(JxlMemoryManager* inner) : inner_(inner) {
+    outer_.opaque = reinterpret_cast<void*>(this);
+    outer_.alloc = &Alloc;
+    outer_.free = &Free;
+  }
+
+  JxlMemoryManager* get() { return &outer_; }
+
+  void PrintStats() const {
+    fprintf(stderr, "Allocations: %" PRIuS " (max bytes in use: %E)\n",
+            static_cast<size_t>(num_allocations_),
+            static_cast<double>(max_bytes_in_use_));
+  }
+
+ private:
+  static void* Alloc(void* opaque, size_t size) {
+    JXL_CHECK(opaque != nullptr);
+    TrackingMemoryManager* self =
+        reinterpret_cast<TrackingMemoryManager*>(opaque);
+    void* result = self->inner_->alloc(self->inner_->opaque, size);
+    if (result != nullptr) {
+      std::lock_guard<std::mutex> guard(self->mutex_);
+      self->num_allocations_++;
+      self->bytes_in_use_ += size;
+      self->max_bytes_in_use_ =
+          std::max(self->max_bytes_in_use_, self->bytes_in_use_);
+      self->allocations_[result] = size;
+    }
+    return result;
+  }
+
+  static void Free(void* opaque, void* address) {
+    JXL_CHECK(opaque != nullptr);
+    if (address == nullptr) return;
+    TrackingMemoryManager* self =
+        reinterpret_cast<TrackingMemoryManager*>(opaque);
+    {
+      std::lock_guard<std::mutex> guard(self->mutex_);
+      auto entry = self->allocations_.find(address);
+      JXL_CHECK(entry != self->allocations_.end());
+      self->num_allocations_--;
+      self->bytes_in_use_ -= entry->second;
+      self->allocations_.erase(entry);
+    }
+    self->inner_->free(self->inner_->opaque, address);
+  }
+
+  std::unordered_map<void*, size_t> allocations_;
+  std::mutex mutex_;
+  uint64_t bytes_in_use_ = 0;
+  uint64_t max_bytes_in_use_ = 0;
+  uint64_t num_allocations_ = 0;
+  JxlMemoryManager outer_;
+  JxlMemoryManager* inner_;
+};
 
 Status WriteImage(const Image3F& image, ThreadPool* pool,
                   const std::string& base_filename) {
@@ -771,13 +831,17 @@ class Benchmark {
   // Return the exit code of the program.
   static int Run() {
     int ret = EXIT_SUCCESS;
+    JxlMemoryManager default_memory_manager;
+    JXL_CHECK(jxl::MemoryManagerInit(&default_memory_manager, nullptr));
+    TrackingMemoryManager memory_manager(&default_memory_manager);
     {
       const StringVec methods = GetMethods();
       const StringVec extra_metrics_names = GetExtraMetricsNames();
       const StringVec extra_metrics_commands = GetExtraMetricsCommands();
       const StringVec fnames = GetFilenames();
       // (non-const because Task.stats are updated)
-      std::vector<Task> tasks = CreateTasks(methods, fnames);
+      std::vector<Task> tasks =
+          CreateTasks(methods, fnames, memory_manager.get());
 
       std::unique_ptr<ThreadPoolInternal> pool;
       std::vector<std::unique_ptr<ThreadPoolInternal>> inner_pools;
@@ -800,6 +864,7 @@ class Benchmark {
       }
     }
 
+    memory_manager.PrintStats();
     return ret;
   }
 
@@ -964,14 +1029,15 @@ class Benchmark {
   }
 
   static std::vector<Task> CreateTasks(const StringVec& methods,
-                                       const StringVec& fnames) {
+                                       const StringVec& fnames,
+                                       JxlMemoryManager* memory_manager) {
     std::vector<Task> tasks;
     tasks.reserve(methods.size() * fnames.size());
     for (size_t idx_image = 0; idx_image < fnames.size(); ++idx_image) {
       for (size_t idx_method = 0; idx_method < methods.size(); ++idx_method) {
         tasks.emplace_back();
         Task& t = tasks.back();
-        t.codec = CreateImageCodec(methods[idx_method]);
+        t.codec = CreateImageCodec(methods[idx_method], memory_manager);
         t.idx_image = idx_image;
         t.idx_method = idx_method;
         // t.stats is default-initialized.