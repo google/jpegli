libjxl/libjxl

Use tracking memory manager in fuzzers, remove ancient memory_limit hack (#3702)
* Fix "typo"

* Use tracking memory manager in fuzzers (remove ancient memory_limit hack)

tools/tracking_memory_manager.cc
@@ -11,13 +11,18 @@
 #include <mutex>
 
 #include "lib/jxl/base/status.h"
+#include "lib/jxl/memory_manager_internal.h"
 
 namespace jpegxl {
 namespace tools {
 
-TrackingMemoryManager::TrackingMemoryManager(JxlMemoryManager* inner,
-                                             uint64_t cap)
-    : cap_(cap), inner_(inner) {
+TrackingMemoryManager::TrackingMemoryManager(uint64_t cap, uint64_t total_cap)
+    : cap_(cap), total_cap_(total_cap) {
+  jxl::Status status = jxl::MemoryManagerInit(&default_, nullptr);
+  JXL_DASSERT(status);
+  (void)status;
+  inner_ = &default_;
+
   outer_.opaque = reinterpret_cast<void*>(this);
   outer_.alloc = &Alloc;
   outer_.free = &Free;
@@ -32,15 +37,26 @@ void* TrackingMemoryManager::Alloc(void* opaque, size_t size) {
       reinterpret_cast<TrackingMemoryManager*>(opaque);
   {
     std::lock_guard<std::mutex> guard(self->numbers_mutex_);
+    uint64_t new_total = self->total_bytes_allocated + size;
+    if (new_total < size ||
+        (self->total_cap_ && new_total > self->total_cap_)) {
+      // Brittle "OOM" - even freeing memory won't undo it.
+      self->total_bytes_allocated = self->total_cap_;
+      self->seen_oom = true;
+      return nullptr;
+    }
     uint64_t new_bytes_in_use = self->bytes_in_use_ + size;
     if (new_bytes_in_use < size ||
         (self->cap_ && new_bytes_in_use > self->cap_)) {
       // Soft "OOM"
+      self->seen_oom = true;
       return nullptr;
     }
-    self->num_allocations++;
+    self->num_allocations_++;
+    self->total_allocations++;
     self->bytes_in_use_ = new_bytes_in_use;
     self->max_bytes_in_use = std::max(self->max_bytes_in_use, new_bytes_in_use);
+    self->total_bytes_allocated = new_total;
   }
   void* result = self->inner_->alloc(self->inner_->opaque, size);
   if (result != nullptr) {
@@ -49,7 +65,8 @@ void* TrackingMemoryManager::Alloc(void* opaque, size_t size) {
   } else {
     // Hard "OOM" - rollback accounting.
     std::lock_guard<std::mutex> guard(self->numbers_mutex_);
-    self->num_allocations--;
+    self->seen_oom = true;
+    self->num_allocations_--;
     self->bytes_in_use_ -= size;
   }
   return result;
@@ -79,10 +96,27 @@ void TrackingMemoryManager::Free(void* opaque, void* address) {
 
   if (found) {
     std::lock_guard<std::mutex> guard(self->numbers_mutex_);
-    self->num_allocations--;
+    self->num_allocations_--;
     self->bytes_in_use_ -= size;
   }
   self->inner_->free(self->inner_->opaque, address);
+}
+
+jxl::Status TrackingMemoryManager::Reset() {
+  if (num_allocations_ != 0) {
+    return JXL_FAILURE("Memory leak");
+  }
+  if (!allocations_.empty()) {
+    return JXL_FAILURE("Internal logic error");
+  }
+  if (bytes_in_use_ != 0) {
+    return JXL_FAILURE("Internal logic error");
+  }
+  seen_oom = false;
+  max_bytes_in_use = 0;
+  total_allocations = 0;
+  total_bytes_allocated = 0;
+  return true;
 }
 
 }  // namespace tools

google/jpegli

(cherry picked from commit 626cb8fb363fe69b5da62a367e54e043271e203a)

tools/tracking_memory_manager.cc
@@ -9,14 +9,19 @@
 #include <mutex>
 
 #include "lib/base/memory_manager.h"
+#include "lib/extras/memory_manager_internal.h"
 #include "lib/base/status.h"
 
 namespace jpegxl {
 namespace tools {
 
-TrackingMemoryManager::TrackingMemoryManager(JxlMemoryManager* inner,
-                                             uint64_t cap)
-    : cap_(cap), inner_(inner) {
+TrackingMemoryManager::TrackingMemoryManager(uint64_t cap, uint64_t total_cap)
+    : cap_(cap), total_cap_(total_cap) {
+  jxl::Status status = jxl::MemoryManagerInit(&default_, nullptr);
+  JXL_DASSERT(status);
+  (void)status;
+  inner_ = &default_;
+
   outer_.opaque = reinterpret_cast<void*>(this);
   outer_.alloc = &Alloc;
   outer_.free = &Free;
@@ -31,15 +36,26 @@ void* TrackingMemoryManager::Alloc(void* opaque, size_t size) {
       reinterpret_cast<TrackingMemoryManager*>(opaque);
   {
     std::lock_guard<std::mutex> guard(self->numbers_mutex_);
+    uint64_t new_total = self->total_bytes_allocated + size;
+    if (new_total < size ||
+        (self->total_cap_ && new_total > self->total_cap_)) {
+      // Brittle "OOM" - even freeing memory won't undo it.
+      self->total_bytes_allocated = self->total_cap_;
+      self->seen_oom = true;
+      return nullptr;
+    }
     uint64_t new_bytes_in_use = self->bytes_in_use_ + size;
     if (new_bytes_in_use < size ||
         (self->cap_ && new_bytes_in_use > self->cap_)) {
       // Soft "OOM"
+      self->seen_oom = true;
       return nullptr;
     }
-    self->num_allocations++;
+    self->num_allocations_++;
+    self->total_allocations++;
     self->bytes_in_use_ = new_bytes_in_use;
     self->max_bytes_in_use = std::max(self->max_bytes_in_use, new_bytes_in_use);
+    self->total_bytes_allocated = new_total;
   }
   void* result = self->inner_->alloc(self->inner_->opaque, size);
   if (result != nullptr) {
@@ -48,7 +64,8 @@ void* TrackingMemoryManager::Alloc(void* opaque, size_t size) {
   } else {
     // Hard "OOM" - rollback accounting.
     std::lock_guard<std::mutex> guard(self->numbers_mutex_);
-    self->num_allocations--;
+    self->seen_oom = true;
+    self->num_allocations_--;
     self->bytes_in_use_ -= size;
   }
   return result;
@@ -78,10 +95,27 @@ void TrackingMemoryManager::Free(void* opaque, void* address) {
 
   if (found) {
     std::lock_guard<std::mutex> guard(self->numbers_mutex_);
-    self->num_allocations--;
+    self->num_allocations_--;
     self->bytes_in_use_ -= size;
   }
   self->inner_->free(self->inner_->opaque, address);
+}
+
+jxl::Status TrackingMemoryManager::Reset() {
+  if (num_allocations_ != 0) {
+    return JXL_FAILURE("Memory leak");
+  }
+  if (!allocations_.empty()) {
+    return JXL_FAILURE("Internal logic error");
+  }
+  if (bytes_in_use_ != 0) {
+    return JXL_FAILURE("Internal logic error");
+  }
+  seen_oom = false;
+  max_bytes_in_use = 0;
+  total_allocations = 0;
+  total_bytes_allocated = 0;
+  return true;
 }
 
 }  // namespace tools