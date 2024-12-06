libjxl/libjxl

Absolutize "using" declarations (#3701)

tools/ssimulacra2.cc
@@ -44,11 +44,11 @@ Design:
 
 namespace {
 
-using jxl::Image3F;
-using jxl::ImageBundle;
-using jxl::ImageF;
-using jxl::Status;
-using jxl::StatusOr;
+using ::jxl::Image3F;
+using ::jxl::ImageBundle;
+using ::jxl::ImageF;
+using ::jxl::Status;
+using ::jxl::StatusOr;
 
 const float kC2 = 0.0009f;
 const int kNumScales = 6;

google/jpegli

(cherry picked from commit f73ce71a606545eae76144ca34bb4ee593144f6e)

tools/ssimulacra2.cc
@@ -48,13 +48,13 @@ Design:
 
 namespace {
 
-using jxl::ColorEncoding;
-using jxl::Image3F;
-using jxl::ImageF;
-using jxl::Rect;
-using jxl::Status;
-using jxl::StatusOr;
-using jxl::extras::PackedPixelFile;
+using ::jxl::ColorEncoding;
+using ::jxl::Image3F;
+using ::jxl::ImageF;
+using ::jxl::Rect;
+using ::jxl::Status;
+using ::jxl::StatusOr;
+using ::jxl::extras::PackedPixelFile;
 
 const float kC2 = 0.0009f;
 const int kNumScales = 6;